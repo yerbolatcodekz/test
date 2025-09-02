87de574daa3de487cab8ce0bcb14e8f4b7dd2386
Yerbolat <yerbolatsultancode@gmail.com>
2025-09-02 09:43:24 +0500

    first test commit

---
+++ another.rs
@@ -0,0 +1,3 @@
+pub fn new() {
+    println!("Hello from new file!");
+}
---
+++ main.rs
@@ -0,0 +1,18 @@
+mod another;
+use std::fmt::Debug;
+
+fn main() {
+    let human = Human { name: "Human1".to_string(), age: 45, gender: Gender::Male, };
+    println!("Hello from Human: {}, age: {}, gender: {:?}. Who lives inside main.rs", human.name, human.age, human.gender);
+    another::new();
+} 
+#[derive(Debug)]
+enum Gender {
+    Male,
+    Famale,
+}
+struct Human {
+    name: String,
+    age: i32,
+    gender: Gender 
+}
