3a57c668676e7266a18c0e7aeb57ddfc3ab8aba0
Yerbolat <yerbolatsultancode@gmail.com>
2025-09-02 10:05:29 +0500

    Added speak of Human about his animal

--- main.rs
+++ main.rs
@@ -3,7 +3,9 @@ use std::fmt::Debug;
 
 fn main() {
     let human = Human { name: "Human1".to_string(), age: 45, gender: Gender::Male, };
+    let animal = Animal { name: "Cow".to_string(), };
     println!("Hello from Human: {}, age: {}, gender: {:?}. Who lives inside main.rs", human.name, human.age, human.gender);
+    println!("I have a animal. It's name is {}", animal.name);
     another::new();
 } 
 #[derive(Debug)]
